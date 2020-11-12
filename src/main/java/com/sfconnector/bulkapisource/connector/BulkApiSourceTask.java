c75c531e9f2e85dabfa3f3d6f68178960c768188
Asit Mahato <asitm9@udzaa.com>
2020-11-12 10:59:34 +0530

    variable restoration from last run -draft 1

--- src/main/java/com/sfconnector/bulkapisource/connector/BulkApiSourceTask.java
+++ src/main/java/com/sfconnector/bulkapisource/connector/BulkApiSourceTask.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -38,6 +39,7 @@ public class BulkApiSourceTask extends SourceTask {
     private String sfdc_query;
     private String topic;
     private String jobId;
+    private String sfLocator;
 
     private Bulk2Client client;
 
@@ -55,10 +57,11 @@ public class BulkApiSourceTask extends SourceTask {
         String sourcesStr = properties.get("sources");
         sources = Arrays.asList(sourcesStr.split(","));
 
-        sfdc_object = config.getString(SFDC_OBJECT_CONFIG);
+        setupTaskConfig(properties);
+        
 
         // try{
-        // setupTaskConfig(properties);
+        // 
         // createQueryJob();
         // }catch(Exception e){
         //     log.error("Exception occured during connection.");
@@ -66,16 +69,6 @@ public class BulkApiSourceTask extends SourceTask {
         //     log.info(e.getStackTrace().toString());
         // }
 
-        
-        Map<String, Object> persistedMap = null;
-
-        if(context !=null && context.offsetStorageReader() !=null ){
-            persistedMap = context.offsetStorageReader().offset(Collections.singletonMap(SFDC_OBJECT_CONFIG, sfdc_object));
-        }
-        log.info("Trying to get persistedMap.");
-        if(persistedMap !=null ){
-            log.info("$$$$$$$$$$$$$$$$$$$$$$$$persistedMap.");
-        }
 
     }
 
@@ -86,12 +79,12 @@ public class BulkApiSourceTask extends SourceTask {
         for (String source : sources) {
             log.info("Polling data from the source '" + source + "'");
             records.add(new SourceRecord(
-                Collections.singletonMap("source", source),
-                Collections.singletonMap("offset", 0),
+                buildSourcePartition(),
+                buildSourceOffset(jobId, sfLocator),
                 source, null, null, null, Schema.BYTES_SCHEMA,
                 String.format("Data from %s", source).getBytes()));
         }
-        buildSourcePartition();
+        
         return records;
 
     }
@@ -101,17 +94,19 @@ public class BulkApiSourceTask extends SourceTask {
     }
 
 
-    private void createQueryJob() throws InterruptedException{
-        
+    private String createQueryJob(Bulk2Client client) throws InterruptedException{
+        String jId = null;
         CreateJobResponse createJobResponse = client.createQuery(null, sfdc_query);
-        String jId = createJobResponse.getId();
+        jId = createJobResponse.getId();
         log.info("***********Job ID***- '" + jId + "'");
-        jobId = jId;
-        Thread.sleep(monitorThreadTimeout);
-        BufferedReader reader = new BufferedReader(client.getQueryResults(jobId, "9", ""));
+
+        return jId;
+        // jobId = jId;
+        // Thread.sleep(monitorThreadTimeout);
+        // BufferedReader reader = new BufferedReader(client.getQueryResults(jobId, "9", ""));
         
-        log.info("Polling data from SF");
-        reader.lines().forEach(s -> log.info(s));
+        // log.info("Polling data from SF");
+        // reader.lines().forEach(s -> log.info(s));
 
     }
     // private List<SourceRecord> getsObjectAsSourceRecords() {
@@ -119,11 +114,23 @@ public class BulkApiSourceTask extends SourceTask {
     // }
     private Map<String, Object> buildSourcePartition(){
         
-        return Collections.singletonMap(SFDC_OBJECT_CONFIG, sfdc_object);
+        Map<String, Object> map = new HashMap<>();
+        map.put(SFDC_OBJECT_CONFIG, sfdc_object);
+        // Some other fields needs to be added to differentiate the orgs.
+        // map.put(USERNAME_CONFIG, username); modification required, uncomment may not work
+        return map;
     }
-    private void setupTaskConfig(Map<String, String> props) throws IOException{
+
+    private Map<String, Object> buildSourceOffset(String jobId, String sfLocator){
+        Map<String, Object> map = new HashMap<>();
+        map.put("JOB_ID", jobId);
+        map.put("SF_LOCATOR", sfLocator);
+        return map;
+    }
+
+    private void setupTaskConfig(Map<String, String> props){
         config = new BulkApiSourceConnectorConfig(props);
-        //baseUrl = config.getString("SFDC_URL_CONFIG");
+        //baseUrl = config.getString(SFDC_URL_CONFIG);
         username = config.getString(USERNAME_CONFIG);
         password = config.getString(PASSWORD_CONFIG);
         sfdc_key = config.getString(SFDC_KEY_CONFIG);
@@ -133,12 +140,67 @@ public class BulkApiSourceTask extends SourceTask {
         topic = config.getString(TOPIC_CONFIG);
 
         log.info("-------> setupTaskConfig config sfdc_key- '" + sfdc_key + "'");
-        
+    }
+
+    private Bulk2Client buildApiClient() throws IOException{
+
+        log.info("-------> setupTaskConfig config sfdc_key- '" + sfdc_key + "'");
         client = new Bulk2ClientBuilder().
                 withPassword(sfdc_key, sfdc_secret, username, password)
                 .build();
-            
-        log.info("Succesfully logged in....");
         
+        log.info("Succesfully logged in....");
+
+        return client;
+
+    }
+
+    private void initLastVars(){
+
+        // Map<String, Object> persistedMap = null;
+
+        // if(context !=null && context.offsetStorageReader() !=null ){
+        //     persistedMap = context.offsetStorageReader().offset(buildSourcePartition());
+        // }
+        // log.info("Trying to get persistedMap.");
+        // if(persistedMap !=null ){
+        //     log.info("$$$$$$$$$$$$$$$$$$$$$$$$persistedMap.");
+        // }
+
+        Map<String, Object> lastSourceOffset = null;
+
+        if(context !=null && context.offsetStorageReader() != null){
+            lastSourceOffset = context.offsetStorageReader().offset(buildSourcePartition());
+        }
+
+        if(lastSourceOffset == null){
+            try{
+                Bulk2Client client = buildApiClient();
+                jobId = createQueryJob(client);
+                sfLocator = "-1";
+                //processQueryResult(client, jobId, sfLocator);
+            }catch(Exception e){}
+            
+        } else{
+            try{
+                Bulk2Client client = buildApiClient();
+
+                Object jId = lastSourceOffset.get("JOB_ID");
+
+                Object sLoc = lastSourceOffset.get("SF_LOCATOR");
+                if(jId != null && (jId instanceof String)){
+                    jobId = String.valueOf(jId);
+                }
+                if(sLoc != null && (sLoc instanceof String)){
+                    sfLocator = String.valueOf(sLoc);
+                }
+
+                //processQueryResult(client, jobId, sfLocator);
+            }catch(Exception e){}
+        }
+    }
+
+    private SourceRecord generateSourceRecord(){
+
     }
 }
